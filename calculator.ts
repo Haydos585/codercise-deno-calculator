a8e2e2ffdb6824c9b631016938cc2471e723144a
Nick Hayden <nickhayden-web@outlook.com>
2021-04-04 09:51:43 +1000

    Handle GET requests and nothing else

--- calculator.ts
+++ calculator.ts
@@ -1,8 +1,31 @@
-addEventListener('fetch', (event) => {
-    const answer = 30 + 12;
-    const response = new Response(`The answer is: ${answer}, or is it?`, {
-        headers: { 'content-type': 'text/plain' }
-    })
+function welcomeMessage() {
+    return new Response("Hello, welcome to Codercise Calculator.", {
+        headers: {
+            'content-type': 'text/plain',
+            'X-codercise': '1234'
+        }
+    });
+}
 
-    event.respondWith(response);
+function errorResponse() {
+    return new Response("We are unable to handle this request. Please try again later", {
+        headers: {
+            'content-type': 'text/plain',
+            'X-codercise': '1234'
+        },
+        status: 405
+    });
+}
+
+function handleRequest(request: any) {
+    if (request?.method === "GET") {
+        return welcomeMessage();
+    }
+
+    return errorResponse();
+}
+
+
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request));
 });
