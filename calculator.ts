6a1c950eeb813bf7ad9fd1eea1111dd58aaa5ebf
Nick Hayden <nickhayden-web@outlook.com>
2021-04-04 10:08:33 +1000

    Add calculation functionality

--- calculator.ts
+++ calculator.ts
@@ -17,9 +17,50 @@ function errorResponse() {
     });
 }
 
-function handleRequest(request: any) {
+function calculate(parameter1: number, parameter2: number, operation: String): number | string {
+    if (operation === "add") {
+        return parameter1 + parameter2;
+    }
+
+    if (operation === "subtract") {
+        return parameter1 - parameter2;
+    }
+
+    if (operation === "multiply") {
+        return parameter1 * parameter2;
+    }
+
+    if (operation === "divide") {
+        return parameter1 / parameter2;
+    }
+
+    return "Error";
+}
+
+async function handleRequest(request: any) {
     if (request?.method === "GET") {
         return welcomeMessage();
+    }
+
+    if (request?.method === "POST") {
+        const requestData = await request.json();
+        const { parameter1, parameter2, operation } = requestData;
+
+        const returnedValue = calculate(parameter1, parameter2, operation);
+        let statusCode = 405;
+
+        if (typeof returnedValue === 'number') {
+            statusCode = 200;
+        }
+
+        return new Response(`${returnedValue}`, {
+            headers: {
+                'content-type': 'text/plain',
+                'X-codercise': '1234'
+            },
+            status: statusCode 
+        })
+
     }
 
     return errorResponse();
